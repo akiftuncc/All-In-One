8124725656a56342103f66b2bb94489de402fb1c
Akif Tunç <86754778+akiftuncc@users.noreply.github.com>
2023-03-27 22:44:50 +0300

    s
    s

--- Atolye Komorebi/script.js
+++ Atolye Komorebi/script.js
@@ -53,3 +53,39 @@ obs.observe(sectionHeroEl);
 const urunler = document.querySelector(".urunler-link");
 
 urunler.addEventListener("mouseenter", () => {});
+//////////////////////////////////////////////////
+
+//  icon
+const iletisim_div = document.querySelector(".body-sticky-div");
+const iletisim_icon = document.querySelector(
+  ".body-sticky-right-bottom ion-icon"
+);
+iletisim_icon.addEventListener("click", function () {
+  if (iletisim_icon.name === "chatbox-outline") {
+    iletisim_icon.setAttribute("name", "close-outline");
+    iletisim_div.classList.remove("display-none");
+  } else {
+    iletisim_icon.setAttribute("name", "chatbox-outline");
+    iletisim_div.classList.add("display-none");
+  }
+});
+
+const iletisimKapatma = document.querySelector(".iletisim-kapatma-iconu");
+
+iletisimKapatma.addEventListener("click", function () {
+  iletisim_icon.setAttribute("name", "chatbox-outline");
+  iletisim_div.classList.add("display-none");
+});
+//////////////////////////////////////////////////////
+
+//URUNLER HOVER
+const hoveredElement = document.querySelector(".urunler-relative");
+const urunlerNav = document.querySelector(".urunler-clicked");
+
+hoveredElement.addEventListener("mouseover", function () {
+  urunlerNav.classList.remove("display-none");
+});
+
+hoveredElement.addEventListener("mouseout", function () {
+  urunlerNav.classList.add("display-none");
+});
