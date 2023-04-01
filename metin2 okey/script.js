f3361e7cc2d45d5e2885f9483f0bcf444d152bc0
Akif Tunç <86754778+akiftuncc@users.noreply.github.com>
2023-04-01 17:12:25 +0300

    x
    x

--- metin2 okey/script.js
+++ metin2 okey/script.js
@@ -1,1 +1,37 @@
- 
+const red_ = document.querySelectorAll(".red");
+const blue_ = document.querySelectorAll(".blue");
+const yellow_ = document.querySelectorAll(".yellow");
+
+const red_final = document.querySelectorAll(".red-final");
+const blue_final = document.querySelectorAll(".blue-final");
+const yellow_final = document.querySelectorAll(".yellow-final");
+
+red_.forEach((element) => {
+  element.addEventListener("click", function () {
+    red_final.forEach((e) => {
+      if (e.textContent == element.textContent) {
+        e.textContent = " ";
+      }
+    });
+  });
+});
+
+blue_.forEach((element) => {
+  element.addEventListener("click", function () {
+    blue_final.forEach((e) => {
+      if (e.textContent == element.textContent) {
+        e.textContent = " ";
+      }
+    });
+  });
+});
+
+yellow_.forEach((element) => {
+  element.addEventListener("click", function () {
+    yellow_final.forEach((e) => {
+      if (e.textContent == element.textContent) {
+        e.textContent = " ";
+      }
+    });
+  });
+});
